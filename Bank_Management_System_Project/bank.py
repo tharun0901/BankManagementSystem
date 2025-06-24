a3cca100364ea58498588e7f50a9e074305d716b
Tharun0901 <moodtharu9@gmail.com>
2025-06-24 10:52:36 +0530

    PEP8 bank_management_system

--- Bank_Management_System_Project/bank.py
+++ Bank_Management_System_Project/bank.py
@@ -1,43 +1,50 @@
 class Bank:
-    def __init__(self,name,age,branch,acno,bal=0.0):
-        self.__name=name
-        self.__age=age
-        self.__branch=branch
-        self.__acno=acno
-        self.__bal=float(bal)
-    def getter(self):
+    def __init__(self, name, age, branch, account_no, balance=0.0):# constructor
+        # variable with private access modifier
+        self.__name = name       
+        self.__age = age        
+        self.__branch = branch   
+        self.__account_no = account_no       
+        self.__balance = float(balance)  
+    def getter(self):     
+        # it is a getter method where the all values return in the form of dictionary 
         return{
-            "name":self.__name,"age":self.__age,"branch":self.__branch,"acno":self.__acno,"bal":self.__bal
-
+            "name":self.__name,
+            "age":self.__age,
+            "branch":self.__branch,
+            "account_no":self.__account_no,
+            "balance":self.__balance
         }
-    def deposit(self,amount):
-        if amount>0:
-            self.__bal=self.__bal+amount
-            print(f"Deposited amount is :{amount} and now total balnace is:{self.__bal}" )
+    def deposit(self, amount):
+        if amount > 0:   # when the client deposit amount more than 0
+            self.__balance += amount  # when the deposited here the balance will be updated
+            print(f"Deposited amount is :{amount} and now total balnace is:{self.__balance}" )
         else:
             print("please deposite amount you have low balance")
-    def withdraw(self,amount):
-        if amount<=self.__bal:
-            self.__bal=self.__bal-amount
-            print(f"withdraw amount is {amount} and your remaining balance is{self.__bal}")
+    def withdraw(self, amount):
+        if amount <= self.__balance: # when the client withdraw amount less than or equal to balace
+            self.__balance -= amount # after the withdraw the remaining balance will be shown
+            print(f"withdraw amount is {amount} and your remaining balance is{self.__balance}")
         else:
             print("you have insufficient balance")
-    def fileWrite(self):
-        file=f"{self.__acno}.txt"
-        with open(file,"w") as f:
-            f.write(f"{self.__name} ")
-            f.write(f"{self.__age} ")
-            f.write(f"{self.__branch} ")
-            f.write(f"{self.__acno} ")
-            f.write(f"{self.__bal} ")
+    def file_write(self):
+        #writing the account details in the files
+        file = f"{self.__account_no}.txt"  
+        with open(file, "w") as f:     
+            f.write(f"{self.__name}\n")  
+            f.write(f"{self.__age}\n")   
+            f.write(f"{self.__branch}\n")  
+            f.write(f"{self.__account_no}\n")
+            f.write(f"{self.__balance}\n")   
     @staticmethod
-    def fileRead(acno):
-        file=f"{acno}.text"
-        with open(file,"r")as f:
-            name = f.read()
-            age = f.read()
-            branch = f.read()
-            acno = f.read()
-            bal = float(f.read())
-            return Bank(name, age, branch, acno, bal)
+    def file_read(account_no):
+        #reading the account details
+        file = f"{account_no}.txt"
+        with open(file,"r")as f: 
+            name = f.readline().strip() 
+            age = f.readline().strip()  
+            branch = f.readline().strip() 
+            account_no = f.readline().strip() 
+            balance= float(f.readline().strip()) 
+            return Bank(name, age, branch, account_no, balance) # it is returning the object with the values
 
--- Bank_Management_System_Project/main.py
+++ Bank_Management_System_Project/main.py
@@ -1,35 +1,35 @@
 from bank import Bank
 class BankManagement:
     def main():
-     account=None
+     account = None
      while True:
         print("1. Create account")
         print("2. Account details")
         print("3. Deposit")
         print("4. Withdraw")
         print("5. cancel")
-        opt=input("select options from the above:")
-        if opt=="1":
-           name=input("Enter your name:")
-           age=input("Enter your age:")
-           branch=input("Enter the branch:")
-           acno=input("Enter the accunt number")
-           account=Bank(name,age,branch,acno)
-           account.fileWrite()
-        elif opt=="2":
+        opt = input("select options from the above:")
+        if opt == "1":
+           name = input("Enter your name:")
+           age = input("Enter your age:")
+           branch = input("Enter the branch:")
+           account_no = input("Enter the accunt number")
+           account = Bank(name,age,branch,account_no)
+           account.file_write()
+        elif opt == "2":
            if account:
-              detail=account.getter()
+              detail = account.getter()
               for k,v in detail.items():
                  print(f"{k}:{v}")
            else:
               print("no account details")
-        elif opt=="3":
+        elif opt == "3":
             amount = float(input("Enter amount you want to deposit: "))
             account.deposit(amount)
-        elif opt=="4":
+        elif opt == "4":
             amount = float(input("Enter amount you want to withdraw: "))
             account.withdraw(amount)
-        elif opt=="5":
+        elif opt == "5":
            print("exit")
            break
         else:
